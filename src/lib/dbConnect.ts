ead476e6d468191d6e2a6c2b01b76d870d6fb5e0
Walid Baharwal <walidbaharwal5400@gmail.com>
2024-05-04 14:37:07 +0500

    feat: added Databse connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+
+const connection: connectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to Database");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log("Error connecting to Database");
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
