6e2f8908d812bc144634f25eb830b42fcb936758
Walid Baharwal <walidbaharwal5400@gmail.com>
2024-05-19 18:07:39 +0500

    refactor: shifted sparkles component from dashboard page to layout.tsx

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { SparklesCore } from "@/components/ui/sparkles"; 
 import { useToast } from "@/components/ui/use-toast";
 import { Message } from "@/models/Message.model";
 import { acceptMessageSchema } from "@/schemas/acceptMessageSchema";
@@ -141,18 +140,7 @@ const Dashboard = () => {
   return (
     <>
       <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6  rounded w-full max-w-6xl">
-      <div style={{zIndex: -1}} className="w-full absolute inset-0 h-screen ">
-      <SparklesCore
-          id="tsparticlesfullpage"
-          background="transparent"
-          minSize={0.6}
-          maxSize={1.4}
-          particleDensity={100}
-          className="w-full h-full"
-          particleColor="#FFFFFF"
-          
-        />
-        </div>
+    
         <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
 
         <div className="mb-4">
--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-
+import { SparklesCore } from "@/components/ui/sparkles";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -9,7 +9,18 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      {children}
+      <div style={{ zIndex: -1 }} className="w-full absolute inset-0 h-screen ">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={20}
+         className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
+        {children}
     </div>
   );
 }
